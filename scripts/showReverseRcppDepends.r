5f42277864d6e3fb6acb3a9953d25af3c0a02eb2
Dirk Eddelbuettel <edd@debian.org>
2014-02-01 16:39:14 -0600

    script updated regarding update of dependent package

--- scripts/showReverseRcppDepends.r
+++ scripts/showReverseRcppDepends.r
@@ -7,10 +7,17 @@ rcppset <- unique(sort(unname(IP[unique(c(grep("Rcpp", as.character(IP[,"Depends
                                           grep("Rcpp", as.character(IP[,"LinkingTo"])),
                                           grep("Rcpp", as.character(IP[,"Imports"])))),"Package"])))
 
-cat("Rcpp depends which can be upgrades:\n")
-print(intersect(rcppset, AP))
+cat("Rcpp depends which can be upgraded:\n")
+print(onCRAN <- intersect(rcppset, AP))
+dput(onCRAN)
 
 cat("\n\nPossibly local / archived / github / bioC ... packages:\n")
-print(setdiff(rcppset, AP))
+print(other <- setdiff(rcppset, AP))
+dput(other)
 
+## edit these to match locaL preferences
+lib <- "/usr/local/lib/R/site-library"
+rep <- "http://cran.rstudio.com"
 
+## after possibly adjusting 'onCRAN' run the next line
+# install.packages(localOnCRAN, lib=lib, repos=rep)
