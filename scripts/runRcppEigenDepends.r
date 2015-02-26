6b00b05c9b9ceecf7fbf1e938147b05f7dd9602f
Dirk Eddelbuettel <edd@debian.org>
2015-02-26 15:11:02 -0600

    small change to script matching Rcpp and RcppArmadillo scripts

--- scripts/runRcppEigenDepends.r
+++ scripts/runRcppEigenDepends.r
@@ -31,6 +31,8 @@ rcppeigenset <- sort(unname(AP[unique(c(grep(pkg, as.character(AP[,"Depends"])),
 print( rcppeigenset )
 
 res <- data.frame(pkg=rcppeigenset, res=NA)
+good <- bad <- 0
+n <- nrow(res)
 
 #for (pi in 1:nrow(res)) {
 #lres <- mclapply(1:nrow(res), mc.cores = 4, FUN=function(pi) {
@@ -60,7 +62,13 @@ lres <- lapply(1:nrow(res), FUN=function(pi) {
     rc <- system(paste("xvfb-run --server-args=\"-screen 0 1024x768x24\" ",
                        "R CMD check --no-manual --no-vignettes ", pkg, " > ", pkg, ".log", sep=""))
     res[pi, "res"] <- rc
-    cat("\n\nRESULT for", pkg, ":", ifelse(rc==0, "success", "failure"), "\n\n\n")
+    if (rc == 0) {
+        good <<- good + 1
+    } else {
+        bad <<- bad + 1
+    }
+    cat(sprintf("\nRESULT for %s : %s (%d of %d, %d good, %d bad)\n",
+                pkg, if (rc==0) "success" else "failure", pi, n, good, bad))
     res[pi, ]
 })
 
